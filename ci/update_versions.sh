4622f4789ef11944aac635bb8a09998d604170f7
Victor Morales <chipahuac@hotmail.com>
2024-11-04 11:47:32 -0800

    Enable KinD upgrade version

--- ci/update_versions.sh
+++ ci/update_versions.sh
@@ -14,6 +14,44 @@ if [[ ${DEBUG:-false} == "true" ]]; then
     set -o xtrace
 fi
 
+last_version=$(curl -sL https://registry.hub.docker.com/v2/repositories/kindest/node/tags | python -c 'import json,sys,re;versions=[obj["name"][1:] for obj in json.load(sys.stdin)["results"] if re.match("^v?(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)$",obj["name"])];print("\n".join(versions))' | uniq | sort -rn | head -n 1)
+
+# editorconfig-checker-disable
+cat <<EOT >scripts/cluster-config.yml
+---
+# SPDX-license-identifier: Apache-2.0
+##############################################################################
+# Copyright (c) 2024-$(date +"%Y")
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+containerdConfigPatches:
+  - |-
+    [plugins."io.containerd.grpc.v1.cri".registry]
+      config_path = "/etc/containerd/certs.d"
+nodes:
+  - role: control-plane
+    image: kindest/node:v$last_version
+  - role: worker
+    image: kindest/node:v$last_version
+  - role: worker
+    image: kindest/node:v$last_version
+  - role: worker
+    image: kindest/node:v$last_version
+  - role: worker
+    image: kindest/node:v$last_version
+  - role: worker
+    image: kindest/node:v$last_version
+  - role: worker
+    image: kindest/node:v$last_version
+EOT
+# editorconfig-checker-enable
+
 # Update GitHub Action commit hashes
 gh_actions=$(grep -r "uses: [a-zA-Z\-]*/[\_a-z\-]*@" .github/ | sed 's/@.*//' | awk -F ': ' '{ print $3 }' | sort -u)
 for action in $gh_actions; do
