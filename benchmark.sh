6d02eb9fb9e925baeac661012a79014a7b243d73
Victor Morales <chipahuac@hotmail.com>
2024-11-07 13:45:22 -0800

    Use fork repo for benchmarks

--- benchmark.sh
+++ benchmark.sh
@@ -17,7 +17,23 @@ fi
 
 source scripts/_utils.sh
 
+function _setup_sysctl {
+    local key="$1"
+    local value="$2"
+
+    if [ "$(sysctl -n "$key")" != "$value" ]; then
+        if [ -d /etc/sysctl.d ]; then
+            echo "$key=$value" | sudo tee "/etc/sysctl.d/99-$key.conf"
+        elif [ -f /etc/sysctl.conf ]; then
+            echo "$key=$value" | sudo tee --append /etc/sysctl.conf
+        fi
+
+        sudo sysctl "$key=$value"
+    fi
+}
+
 # Install requeriments
+echo "::group::Install requriments"
 if ! command -v docker >/dev/null; then
     export PKG="docker"
 fi
@@ -28,9 +44,25 @@ if ! command -v nerdctl >/dev/null; then
     curl -s "https://i.jpillora.com/containerd/nerdctl!" | bash
 fi
 
-[ ! -d /opt/hello-bench ] && sudo git clone https://github.com/nydusaccelerator/hello-bench/ /opt/hello-bench
+[ ! -d /opt/hello-bench ] && sudo git clone https://github.com/electrocucaracha/hello-bench/ /opt/hello-bench
 
-for image in python:3 alpine:3 ubuntu:20.04 ubuntu:22.04; do
-    sudo /opt/hello-bench/hello.py --engine docker --op run --images "$image" >/dev/null
-    info "$image pull elapsed: $(jq '[ with_entries(select(.key | startswith("pull_elapsed", "total_elapsed"))) | .[] | tonumber ] | sort | (.[0]/.[1])*100' bench.json)%"
-done
+_setup_sysctl "vm.max_map_count" "262144"
+echo "::endgroup::"
+
+# Get the metrics
+echo "::group::Get metrics"
+sudo /opt/hello-bench/hello.py --engine docker --op run --images python:3 alpine:3 ubuntu:20.04 ubuntu:22.04 >/dev/null
+echo "::endgroup::"
+
+# Display the metrics
+while IFS= read -r line; do
+    repo=$(echo "$line" | jq -r '.bench')
+    pull=$(echo "$line" | jq '[ with_entries(select(.key | startswith("pull_elapsed", "total_elapsed"))) | .[] | tonumber ] | sort | (.[0]/.[1])*100 | .*100 | round/100')
+    create=$(echo "$line" | jq '[ with_entries(select(.key | startswith("create_elapsed", "total_elapsed"))) | .[] | tonumber ] | sort | (.[0]/.[1])*100 | .*100 | round/100')
+    run=$(echo "$line" | jq '[ with_entries(select(.key | startswith("run_elapsed", "total_elapsed"))) | .[] | tonumber ] | sort | (.[0]/.[1])*100 | .*100 | round/100')
+    echo "::group::$repo stats"
+    info "Pull: $pull%"
+    info "Create: $create%"
+    info "Run: $run%"
+    echo "::endgroup::"
+done <bench.json
