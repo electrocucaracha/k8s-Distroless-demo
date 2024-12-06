fa2e67f5670f186871aa08790a0a8ffa017981ec
Victor Morales <chipahuac@hotmail.com>
2024-12-05 19:23:23 -0800

    Add get status function for demo troubleshooting

--- demo.sh
+++ demo.sh
@@ -17,7 +17,38 @@ fi
 
 source scripts/_utils.sh
 
-trap 'kubectl delete deployments/java-server --ignore-not-found' EXIT
+# get_status() - Print the current status of the cluster
+function get_status {
+    set +o xtrace
+    if [ -f /proc/stat ]; then
+        printf "CPU usage: "
+        grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print usage " %"}'
+    fi
+    if [ -f /proc/pressure/io ]; then
+        printf "I/O Pressure Stall Information (PSI): "
+        grep full /proc/pressure/io | awk '{ sub(/avg300=/, ""); print $4 }'
+    fi
+    printf "Memory free(Kb):"
+    if [ -f /proc/zoneinfo ]; then
+        awk -v low="$(grep low /proc/zoneinfo | awk '{k+=$2}END{print k}')" '{a[$1]=$2}  END{ print a["MemFree:"]+a["Active(file):"]+a["Inactive(file):"]+a["SReclaimable:"]-(12*low);}' /proc/meminfo
+    fi
+    printf "Disk usage: "
+    sudo df -h
+    if command -v docker >/dev/null; then
+        echo "Docker statistics:"
+        docker stats --no-stream
+        docker ps --size
+    fi
+
+    echo "Kubernetes Events:"
+    kubectl get events -A --sort-by=".metadata.managedFields[0].time"
+    echo "Kubernetes Resources:"
+    kubectl get all -A -o wide
+    echo "Kubernetes Pods:"
+    kubectl describe pods
+    echo "Kubernetes Nodes:"
+    kubectl describe nodes
+}
 
 function _get_stats {
     before=$(curl -s http://127.0.0.1:5001/status/format/json | jq '.upstreamZones["::nogroups"][0].outBytes')
@@ -28,6 +59,9 @@ function _get_stats {
     data_transf=$((after - before))
     info "Data Transfer: $(printf "%sB\nMB" "$data_transf" | units --quiet --one-line --compact)MB"
 }
+
+trap get_status ERR
+trap 'kubectl delete deployments/java-server --ignore-not-found' EXIT
 
 # Deploy initial version
 info "Deploy version 1 of java web server"
